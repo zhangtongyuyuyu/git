8a0ce33c737e7e6305eec5bd09274456981de12c
zhangtongyuyuyu <2695787740@qq.com>
2020-09-26 11:25:03 +0800

    ok

--- src/main/java/com/example/git1/controller/indexController.java
+++ src/main/java/com/example/git1/controller/indexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("index")
 public class indexController {
 
-    @RequestMapping("hello")
+    @RequestMapping("helloa")
     public String index(){
         return "index";
     }
